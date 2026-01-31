d2779adcc83256707909b03915ad79063b5a357e
Dhia1111 <dhia23445@gmail.com>
2026-01-31 23:19:40 +0100

    fix(backend): delete var , unused var

--- backend/APIs/Controllers/WeatherForecastController.cs
+++ backend/APIs/Controllers/WeatherForecastController.cs
@@ -21,7 +21,6 @@ namespace APIs.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            int l=0;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
