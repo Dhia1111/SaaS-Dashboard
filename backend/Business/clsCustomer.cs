cfb6a3bf8071912b75b978a9027e4439963143be
Dhia1111 <dhia23445@gmail.com>
2026-02-03 15:33:56 +0100

    fix(gitignore): add some files to gitignore

---
+++ backend/Business/clsCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface ICustomerService {
+
+
+    public List<DTOCustomer> GetAllCustomers();
+
+
+}
+
+namespace Business
+{
+    public class clsCustomer:ICustomerService
+    {
+
+        private readonly ICustomerRepo _customer ;
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Adress { get; set; }
+        public string? phone { get; set; }
+        
+        public clsCustomer(ICustomerRepo _customer)
+        {
+            this._customer = _customer;
+        }
+
+        public List<DTOCustomer> GetAllCustomers()
+        {
+            return _customer.GetAllCustomers();
+        }
+
+    }
+}
