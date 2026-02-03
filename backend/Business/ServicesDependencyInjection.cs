885fe8384f60b121b666dee3fcbab848cd90a066
Dhia1111 <dhia23445@gmail.com>
2026-02-03 14:42:36 +0100

    fix(gitignore): add some files to gitignore

---
+++ backend/Business/ServicesDependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+
+    public static class ServicesDependencyInjection
+    {
+        public static IServiceCollection AddBusinessDependencies(this IServiceCollection services)
+        {
+   services.AddScoped<ICustomerService, clsCustomer>();
+
+            // business rules, domain services, validators, etc.
+
+            return services;
+        }
+    }
+
+}
---
+++ backend/Connection/ConnectionDependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Connection.models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection
+{
+
+    public static class ConnectionDependencyInjection
+    {
+        public static IServiceCollection AddConnectionDependencies(this IServiceCollection services)
+        {
+          // services.AddScoped<ICustomerRepo, clsCustomerRepository>();
+
+            return services;
+        }
+    }
+
+}
