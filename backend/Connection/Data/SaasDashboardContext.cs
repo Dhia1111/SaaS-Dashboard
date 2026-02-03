2087522e21ce29abbef01f4a244be58eea39be4d
Dhia1111 <dhia23445@gmail.com>
2026-02-03 13:52:05 +0100

    chore(backend/db):Entity frame work setup

---
+++ backend/Connection/Data/SaasDashboardContext.cs
@@ -0,0 +1,21 @@
+﻿using Connection.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection.Data
+{
+    public class SaasDashboardContext:DbContext
+    {
+        public virtual DbSet<clsCustomerRepository> Customers { get; set; } = null!;
+
+        public SaasDashboardContext(DbContextOptions<SaasDashboardContext> options):base(options) {
+
+    }
+
+    }
+}
