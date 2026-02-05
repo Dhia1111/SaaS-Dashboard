0243fbceb53b6f11b771b7eab0a799b065a6d2a2
Dhia1111 <dhia23445@gmail.com>
2026-02-05 19:33:15 +0100

    fix(backend): fix spacing and unUsed var issue

--- backend/ExternalAPI/ExternalAPIDependencyInjection.cs
+++ backend/ExternalAPI/ExternalAPIDependencyInjection.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ExternalAPI
 {
 
     public static class ExternalAPIDependencyInjection
     {
-        public static IServiceCollection AddExternalAPI(this IServiceCollection services)
+        public static IServiceCollection AddExternalAPIDependencies(this IServiceCollection services)
         {
 //            services.AddScoped<Ic, clsCustomer>();
 
