d9801521d30ef5784b26cf509e325fb71ea49aed
Dhia1111 <dhia23445@gmail.com>
2026-02-14 16:09:48 +0100

    feat(backend/EF/Db): Create a new Entity class

---
+++ backend/Connection/models/Entites/Tenant.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Connection.models
+{ 
+
+    public class Tenant
+    {
+           [Key]
+           [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; } // PK, identity(1,1)
+            [Required]
+            public int DataKey { get; set; } // required
+          [Required]
+            public int PersonID { get; set; } // required
+            public string PasswordHash { get; set; } = null!;
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+            public DateTime? UpdatedAt { get; set; }
+            public Person Person { get; set; }= null!;
+        
+
+        }
+    }
+
+
