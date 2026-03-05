50eec8712f1e0366424e865a61a270d1ba06b43d
Dhia1111 <dhia23445@gmail.com>
2026-03-05 13:57:31 +0100

    feat(backend): Create a genral responce class to make it easy for the client to read and understand the responces

---
+++ backend/APIs/Responses/ApiResult.cs
@@ -0,0 +1,17 @@
+﻿namespace APIs.Responses
+
+{
+    public class ApiResult<T>
+    {
+        public bool Success { get; init; }
+        public string Code { get; init; } = default!;
+        public string Message { get; init; } = default!;
+        public T? Data { get; init; }
+
+        public static ApiResult<T> Ok(T data, string message = "Success") =>
+            new() { Success = true, Code = "OK", Message = message, Data = data };
+
+        public static ApiResult<T> Fail(string code, string message) =>
+            new() { Success = false, Code = code, Message = message };
+    }
+}
