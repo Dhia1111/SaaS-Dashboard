3a95c7395750aa94039376180de4a8550d942151
Dhia1111 <dhia23445@gmail.com>
2026-05-02 15:23:13 +0100

    chore(backend): create a genral failuer responce for constency

---
+++ backend/APIs/Responses/ApiProblemDetails.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIs.Responses
+{
+    public class ApiProblemDetails : ProblemDetails
+    {
+        public string? TraceId { get; set; }
+        public Dictionary<string, string[]>? Errors { get; set; }
+    }
+}
