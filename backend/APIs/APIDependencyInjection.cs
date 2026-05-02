b7fa0b715314981d9cd7eb965b9662ab182acdd3
Dhia1111 <dhia23445@gmail.com>
2026-05-02 15:16:33 +0100

    chore(backend): register dependencies and thier life time

--- backend/APIs/APIDependencyInjection.cs
+++ backend/APIs/APIDependencyInjection.cs
@@ -1,28 +1,31 @@
 ﻿
 using APIs.AssetHandler;
+using APIs.BackGroundJobs;
 using APIs.Hashing;
+using APIs.TokenHandler;
 using Business;
+using Connection.models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Net.Mail;
- 
+using Quartz;
+using Quartz.Serialization.Json;
+
+
 
 namespace ExternalAPI
 {
 
     public static class APIDependencyInjection
     {
-        
-
-
         public static IServiceCollection AddAPIDependencies(this IServiceCollection services)
         {
-             services.AddScoped<IEmailTemplateHandler, EmailTemplateHandler>();
+            services.AddScoped<IEmailTemplateHandler, EmailTemplateHandler>();
             services.AddScoped<IPasswordHashService, PasswordHashService>();
+            services.AddSingleton<IGenralHashService, GenralHashService>();
             services.AddScoped<IPasswordHasher<object>, PasswordHasher<object>>();
-
+            services.AddScoped<ITokenHandler, TokenHandler>();
+            services.AddTransient<EmailBackgroundJob>();
+            services.AddHttpContextAccessor();
+            services.AddScoped<ITenantIdProvider, APIs.ConfigClasses.TenantIdProvider>();
             return services;
         }
    
