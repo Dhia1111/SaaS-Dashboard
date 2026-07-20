433c28cf9e12af752f1754417100281da3eaddd1
Dhia1111 <dhia23445@gmail.com>
2026-07-20 17:34:42 +0100

    chore(backend): create a config class to read platform info

---
+++ backend/Business/Config/PlatformInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Config
+{
+    public class PlatformInfo
+    {
+        [Required]
+        public string TenantName { get; set; } = null!;
+        public string DefaultCurrency { get; set; } = null!;
+
+    }
+}
