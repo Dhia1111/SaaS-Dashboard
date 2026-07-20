a78553ef0e3efbe89223b6fa4619d9080b784598
Dhia1111 <dhia23445@gmail.com>
2026-07-20 23:22:48 +0100

    feat(tenant): add tenant management APIs

---
+++ backend/APIs/Controllers/Tenant.cs
@@ -0,0 +1,35 @@
+﻿using APIs.Responses;
+using Business;
+using Connection.models;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit.Tnef;
+
+namespace APIs.Controllers
+{
+    [ApiController]
+    [Route("api/tenat")]
+    public class TenantController : ControllerBase
+    {
+        private readonly ITenantService _tenantService;
+        private readonly ITenantIdProvider _tenantIdProvider;
+
+        public TenantController(
+            ITenantService tenantervice,
+            ITenantIdProvider tenantIdProvider)
+        {
+            _tenantService = tenantervice;
+            _tenantIdProvider = tenantIdProvider;
+        }
+
+        [HttpGet("tenant")]
+        public async Task<ActionResult<ApiResult<DtoTenant>>> GetById ([FromQuery] int TenantId)
+        {
+
+                      var tenant = await _tenantService.GetByIdAsync(TenantId);
+            tenant.PasswordHash = null;
+            return Ok(ApiResult<DtoTenant>.Ok(tenant, "Tenant fetched successfully"));
+        }
+     
+
+    }
+}
---
+++ frontend/src/Apis/Tenant.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { store } from "../store";
+import { refreshToken } from "./GenralAuth.js";
+import{RetryPolicies,executeWithRetry} from './RetryPolicy/RetryPolicy.js'
+import { Redirect } from "./RedirectPolicy/RedirectPolicy.js";
+
+var Redirecting=false;
+const Tenant = axios.create({
+  baseURL: "http://localhost:7073/api/tenat",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+Tenant.interceptors.request.use(async (config) => {
+
+  let token = store.getState().auth.accessToken;
+
+  if (!token) {
+      const res = await refreshToken();
+      if(res.success){
+        token=res.data;
+      }
+      else{
+        token=null;
+        Redirect(res.status,Redirecting);
+        Redirecting=true;
+
+
+
+        
+      }
+
+    
+  }
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  
+
+  return config;
+});
+
+export const GetTenatInfoAsync = async (id) => {
+  try {
+    const res = await executeWithRetry(() => Tenant.get(`/tenant`,{params:{TenantId:id}}), RetryPolicies.ReadFast);
+    return { data: res.data.data, message: "User found successfully.", success: true };
+  } catch {
+    return {
+      success: false,
+      message: "Failed to find user.",
+      data: null
+    };
+  }
+};
