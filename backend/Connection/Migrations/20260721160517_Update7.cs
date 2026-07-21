4a0a5a5160b2d066b343f7f41e7291d842c5800d
Dhia1111 <dhia23445@gmail.com>
2026-07-21 17:51:52 +0100

    chore(backend\db): register migrations

---
+++ backend/Connection/Migrations/20260721160517_Update7.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Connection.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlatformSubscriptions_TenantPlanPricingOptionId",
+                table: "PlatformSubscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlatformSubscriptions_TenantPlanPricingOptionId",
+                table: "PlatformSubscriptions",
+                column: "TenantPlanPricingOptionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlatformSubscriptions_TenantPlanPricingOptionId",
+                table: "PlatformSubscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlatformSubscriptions_TenantPlanPricingOptionId",
+                table: "PlatformSubscriptions",
+                column: "TenantPlanPricingOptionId",
+                unique: true);
+        }
+    }
+}
