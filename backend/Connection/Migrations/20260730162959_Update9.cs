0a76a7514fc4a319c4febbc648ec9880bd37c106
Dhia1111 <dhia23445@gmail.com>
2026-07-30 17:33:17 +0100

    feat(subDomainRouting-service): create constraint on the Domain entity

---
+++ backend/Connection/Migrations/20260730162959_Update9.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Connection.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Domains_TenantId",
+                table: "Domains");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Domains_Name",
+                table: "Domains",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Domains_TenantId",
+                table: "Domains",
+                column: "TenantId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Domains_Name",
+                table: "Domains");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Domains_TenantId",
+                table: "Domains");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Domains_TenantId",
+                table: "Domains",
+                column: "TenantId");
+        }
+    }
+}
